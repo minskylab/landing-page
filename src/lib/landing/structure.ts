b44f3d3a117bedf596a6dd9ccf471cf3d3d9a9e7
eltruforce <lordace550@gmail.com>
2022-04-27 16:25:08 -0500

    fix: link of footer info

--- src/lib/landing/structure.ts
+++ src/lib/landing/structure.ts
@@ -1,18 +1,18 @@
-export type MinskyArtType = "illustration" | "video" | "organism" | "system";
-export type MinskySectionType = "about-us" | "our-work" | "our-team" | "contact-us";
+export type MinskyArtType = "illustration" | "video" | "organism" | "system"; // Settings y Content Listo
+export type MinskySectionType = "about-us" | "our-work" | "our-team" | "contact-us"; // Settings y Content Listo
 
-export type MinskyLabelLink = {
+export type MinskyLabelLink = { // Settings y Content Listo
   link: string;
   label: string;
 };
 
-export type MinskyHeaderTopic = {
+export type MinskyHeaderTopic = { // Settings y Content Listo
   link: string;
   label: string;
   links?: MinskyLabelLink[];
 };
 
-export type MinskyLandingSection = {
+export type MinskyLandingSection = { // Settings y Content Listo
   type: MinskySectionType;
   title: string;
   description: string | string[];
@@ -20,37 +20,37 @@ export type MinskyLandingSection = {
   artType?: MinskyArtType;
 };
 
-export type MinskyFAQ = {
+export type MinskyFAQ = { // Settings y Content Listo
   question: string;
   answer: string;
 };
 
-export type MinskyContactInformation = {
+export type MinskyContactInformation = { // Settings y Content Listo
   email: string;
   phone: string;
   address: string;
   workingHours: string;
 };
 
-export type MinskyFooterSection = {
+export type MinskyFooterSection = { // Settings y Content Listo
   title: string;
   links?: MinskyLabelLink[];
 };
 
-export type MinskySocialNetworkKind = "github" | "twitter" | "instagram" | "youtube";
+export type MinskySocialNetworkKind = "github" | "twitter" | "instagram" | "youtube"; // Settings y Content Listo
 
-export type MinskySocialNetwork = {
+export type MinskySocialNetwork = { // Settings y Content Listo
   kind: MinskySocialNetworkKind;
   link: string;
 };
 
-export type MinskyFooterInformation = {
+export type MinskyFooterInformation = { // Settings y Content Listo
   tagline: string;
   sections: MinskyFooterSection[];
   brandTrademark: string;
 };
 
-export type MinskyLandingData = {
+export type MinskyLandingData = { // Settings y Content Listo
   headerTopics: MinskyHeaderTopic[];
   headline: string;
   headlineHighlight: string | string[];
