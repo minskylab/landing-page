82b9873e2e6932d0f6225508f6399ae63b1fffe0
Anna <ana84168@gmail.com>
2023-03-07 12:26:53 -0500

    feat: add portfolio page

---
+++ src/modules/portfolio/PortfolioPage.tsx
@@ -0,0 +1,10 @@
+import { Container } from "@mantine/core";
+import MinskyAlertPageUnderConstruction from "components/ui/AlertPageUnderConstruction";
+
+export default function PortfolioPageContent() {
+  return (
+    <Container>
+      <MinskyAlertPageUnderConstruction />
+    </Container>
+  );
+}
---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,13 @@
+import PortfolioPageContent from "modules/portfolio/PortfolioPage";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale as string, ["common"])),
+    },
+  };
+};
+
+export default PortfolioPageContent;
