087587e5ae733955864095d8f67c280987e1bfba
Anna <ana84168@gmail.com>
2024-03-20 15:21:32 -0500

    feat: add services page and menu

--- src/modules/services/ServicesPage.tsx
+++ src/modules/services/ServicesPage.tsx
@@ -1,27 +1,21 @@
-import { Container, Text, createStyles } from "@mantine/core";
-
-const useStyles = createStyles(theme => {
-  return {
-    mainTitle: {
-      fontSize: 38,
-      fontFamily: `${theme.fontFamily}`,
-      lineHeight: 1.2,
-      [theme.fn.smallerThan("md")]: {
-        fontSize: 34,
-        padding: "40px 20px",
-      },
-    },
-  };
-});
+import { Container, Space, useMantineTheme } from "@mantine/core";
+import { ServicesGrid } from "components/ui/Services";
 
 export default function ServicesPageContent() {
-  const { classes } = useStyles();
-
+  const theme = useMantineTheme();
   return (
-    <Container>
-      <Text pt={40} pb={40} weight={"bold"} className={classes.mainTitle}>
-        Servicios
-      </Text>
+    <Container
+      mt={60}
+      sx={theme => ({
+        [theme.fn.smallerThan("md")]: {
+          paddingLeft: theme.spacing.xl,
+          paddingRight: theme.spacing.xl,
+        },
+      })}
+    >
+      <Space h={`calc(${theme.spacing.xl} * 6)`} />
+      <ServicesGrid />
+      <Space h={`calc(${theme.spacing.xl} * 6)`} />
     </Container>
   );
 }
