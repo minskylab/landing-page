3eaadf92ddb0881522c1d704e898667f512d58bb
Anna <ana84168@gmail.com>
2024-04-11 19:13:40 -0500

    wip: landing page service

---
+++ src/assets/images/projects/index.ts
@@ -0,0 +1,39 @@
+import page1 from "./WebPage-1.png";
+import page2 from "./WebPage2.png";
+import page3 from "./WebPage3.png";
+import page4 from "./WebPage4.png";
+import page5 from "./WebPage5.png";
+import page6 from "./WebPage6.png";
+
+export const projectImages = [
+  {
+    id: "page1",
+    image: page1,
+    height: 80,
+  },
+  {
+    id: "page2",
+    image: page2,
+    height: 80,
+  },
+  {
+    id: "page3",
+    image: page3,
+    height: 80,
+  },
+  {
+    id: "page4",
+    image: page4,
+    height: 80,
+  },
+  {
+    id: "page5",
+    image: page5,
+    height: 80,
+  },
+  {
+    id: "page6",
+    image: page6,
+    height: 80,
+  },
+];
--- src/modules/services/LandingPage.tsx
+++ src/modules/services/LandingPage.tsx
@@ -90,19 +90,20 @@ const useStyles = createStyles(theme => {
       borderRadius: theme.radius.sm,
       padding: `${rem(4)} ${rem(12)}`,
     },
-    wrapper: {
-      paddingTop: `calc(${theme.spacing.xl} * 4)`,
-      paddingBottom: `calc(${theme.spacing.xl} * 4)`,
-    },
     titleProps: {
+      marginBottom: `calc(${theme.spacing.xl} * 2)`,
       fontWeight: 900,
-      marginBottom: theme.spacing.md,
       textAlign: "center",
 
-      [`@media (max-width: ${theme.breakpoints.xs}px)`]: {
+      [theme.fn.smallerThan("xs")]: {
         fontSize: rem(28),
         textAlign: "left",
       },
+
+      /* [`@media (max-width: ${theme.breakpoints.xs}px)`]: {
+        fontSize: rem(28),
+        textAlign: "right",
+      }, */
     },
     description: {
       textAlign: "center",
@@ -118,10 +119,12 @@ const useStyles = createStyles(theme => {
         theme.colorScheme === "dark" ? theme.colors.dark[3] : theme.colors.gray[3]
       }`,
     },
-    titleFaq: {
-      fontWeight: 900,
-      fontFamily: "Open Sans",
-      marginBottom: `calc(${theme.spacing.xl} * 2)`,
+    projectText: {
+      textAlign: "center",
+
+      [theme.fn.smallerThan("xs")]: {
+        textAlign: "left",
+      },
     },
     pricingRow: {
       borderBottom: `1px solid ${
@@ -174,7 +177,7 @@ function Difference({ icon, title, description }: FeatureProps) {
 }
 
 export default function LandingPageContent() {
-  const { classes, theme } = useStyles();
+  const { classes } = useStyles();
   const { t } = useTranslation("services");
 
   const differences: LandingPageDifferences[] = t("landingPage.differences", {
@@ -183,6 +186,9 @@ export default function LandingPageContent() {
   const faqs: LandingPageFAQ[] = t("landingPage.faqs", { returnObjects: true });
   const plans: LandingPagePlans[] = t("landingPage.pricing.plans", { returnObjects: true });
   const plansFeatures: LandingPagePlansFeatures[] = t("landingPage.pricing.features", {
+    returnObjects: true,
+  });
+  const projectsText: string[] = t("landingPage.projectsText", {
     returnObjects: true,
   });
 
@@ -215,11 +221,12 @@ export default function LandingPageContent() {
           alt="landing page"
         />
       </Group>
-      <Container className={classes.wrapper}>
+
+      <Stack className={classes.inner}>
         <Title className={classes.titleProps}>{t("landingPage.differencesTitle")}</Title>
 
         <SimpleGrid
-          mt={60}
+          mt={"xl"}
           breakpoints={[
             { minWidth: "base", cols: 1, spacing: "xl", verticalSpacing: "xl" },
             { minWidth: "sm", cols: 2, spacing: "xl", verticalSpacing: "xl" },
@@ -230,112 +237,129 @@ export default function LandingPageContent() {
             <Difference icon={icon} title={title} description={description} key={title} />
           ))}
         </SimpleGrid>
+      </Stack>
 
-        <Container size="sm" className={classes.wrapper}>
-          <Title align="center" className={classes.titleFaq}>
-            {t("landingPage.faqsTitle")}
-          </Title>
+      <Stack m={"auto"} className={classes.inner} sx={{ maxWidth: 700 }} spacing={0}>
+        <Title className={classes.titleProps}>{t("landingPage.projectsTitle")}</Title>
+        {projectsText.map((item, index) => (
+          <Text key={index} mt="md" className={classes.projectText}>
+            {item}
+          </Text>
+        ))}
 
-          <Accordion
-            classNames={{
-              item: classes.itemFaq,
-              control: classes.control,
-            }}
-          >
-            {faqs.map(({ title, description }: LandingPageFAQ) => (
-              <Accordion.Item key={title} value={title}>
-                <Accordion.Control>{title}</Accordion.Control>
-                <Accordion.Panel>
-                  {description.map((item, key) => {
-                    return (
-                      <Text key={key} pb={"xs"}>
-                        {item}
-                      </Text>
-                    );
-                  })}
-                </Accordion.Panel>
-              </Accordion.Item>
-            ))}
-          </Accordion>
-        </Container>
-        <ScrollArea miw={800} type="never">
-          <SimpleGrid cols={4} px={"md"} py={"lg"} className={classes.pricingRow}>
-            <Stack></Stack>
-            {plans.map(({ id, name, price, monthlyPrice, button }: LandingPagePlans) => (
-              <Stack key={name}>
-                <Text fw={700} color="brand.5">
-                  {name}
-                </Text>
-                {id === "Profesional" ? (
-                  <Stack justify="center" sx={{ flex: 1 }}>
-                    <Group>
-                      <Button
-                        size="sm"
-                        component="a"
-                        href="https://wa.me/+51936445786?text=Hola%20Minsky,%20soy%20[nombre],%20estoy%20buscando%20información%20sobre%20el%20servicio%20de%20desarrollo%20de%20Páginas%20Web,%20me%20interesa%20el%20plan%20[Startup/Profesional/Premium]."
-                        target="_blank"
-                        leftIcon={<BrandWhatsapp size={22} />}
-                      >
-                        {button}
-                      </Button>
-                    </Group>
-                  </Stack>
-                ) : (
-                  <Stack spacing={4}>
-                    <Title order={2} fz={36}>
-                      {price}
-                    </Title>
+        {/* <Image height={100} src={"./page1.png"} alt={"photo"} />
+        <Image
+          height={100}
+          src={
+            "https://images.unsplash.com/photo-1627552245715-77d79bbf6fe2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=640&q=80"
+          }
+          alt={"photo"}
+        /> */}
+      </Stack>
 
-                    <Text fz={"sm"}>+ {monthlyPrice}</Text>
-                  </Stack>
-                )}
-              </Stack>
-            ))}
-          </SimpleGrid>
-          {plansFeatures.map(({ name, basic, professional, premium }: LandingPagePlansFeatures) => (
-            <SimpleGrid key={name} cols={4} px={"md"} py={"lg"} className={classes.pricingRow}>
-              <Stack>
-                <Text size="sm">{name}</Text>
-              </Stack>
-              <Stack>
-                <List
-                  size="sm"
-                  classNames={{
-                    itemWrapper: classes.listItemWrapper,
-                  }}
-                >
-                  {basic.map((item, index) => {
-                    return <List.Item key={index}>{item}</List.Item>;
-                  })}
-                </List>
-              </Stack>
-              <Stack>
-                <List
-                  size="sm"
-                  classNames={{
-                    itemWrapper: classes.listItemWrapper,
-                  }}
-                >
-                  {professional.map((item, index) => {
-                    return <List.Item key={index}>{item}</List.Item>;
-                  })}
-                </List>
-              </Stack>
-              <Stack>
-                <List
-                  size="sm"
-                  classNames={{
-                    itemWrapper: classes.listItemWrapper,
-                  }}
-                >
-                  {premium.map((item, index) => {
-                    return <List.Item key={index}>{item}</List.Item>;
-                  })}
-                </List>
-              </Stack>
-            </SimpleGrid>
+      <ScrollArea miw={800} type="never" className={classes.inner}>
+        <SimpleGrid cols={4} px={"md"} py={"lg"} className={classes.pricingRow}>
+          <Stack></Stack>
+          {plans.map(({ id, name, price, monthlyPrice, button }: LandingPagePlans) => (
+            <Stack key={name}>
+              <Text fw={700} color="brand.5">
+                {name}
+              </Text>
+              {id === "Profesional" ? (
+                <Stack justify="center" sx={{ flex: 1 }}>
+                  <Group>
+                    <Button
+                      size="sm"
+                      component="a"
+                      href="https://wa.me/+51936445786?text=Hola%20Minsky,%20soy%20[nombre],%20estoy%20buscando%20información%20sobre%20el%20servicio%20de%20desarrollo%20de%20Páginas%20Web,%20me%20interesa%20el%20plan%20[Startup/Profesional/Premium]."
+                      target="_blank"
+                      leftIcon={<BrandWhatsapp size={22} />}
+                    >
+                      {button}
+                    </Button>
+                  </Group>
+                </Stack>
+              ) : (
+                <Stack spacing={4}>
+                  <Title order={2} fz={36}>
+                    {price}
+                  </Title>
+
+                  <Text fz={"sm"}>+ {monthlyPrice}</Text>
+                </Stack>
+              )}
+            </Stack>
           ))}
-        </ScrollArea>
+        </SimpleGrid>
+        {plansFeatures.map(({ name, basic, professional, premium }: LandingPagePlansFeatures) => (
+          <SimpleGrid key={name} cols={4} px={"md"} py={"lg"} className={classes.pricingRow}>
+            <Stack>
+              <Text size="sm">{name}</Text>
+            </Stack>
+            <Stack>
+              <List
+                size="sm"
+                classNames={{
+                  itemWrapper: classes.listItemWrapper,
+                }}
+              >
+                {basic.map((item, index) => {
+                  return <List.Item key={index}>{item}</List.Item>;
+                })}
+              </List>
+            </Stack>
+            <Stack>
+              <List
+                size="sm"
+                classNames={{
+                  itemWrapper: classes.listItemWrapper,
+                }}
+              >
+                {professional.map((item, index) => {
+                  return <List.Item key={index}>{item}</List.Item>;
+                })}
+              </List>
+            </Stack>
+            <Stack>
+              <List
+                size="sm"
+                classNames={{
+                  itemWrapper: classes.listItemWrapper,
+                }}
+              >
+                {premium.map((item, index) => {
+                  return <List.Item key={index}>{item}</List.Item>;
+                })}
+              </List>
+            </Stack>
+          </SimpleGrid>
+        ))}
+      </ScrollArea>
+
+      <Container size="sm" className={classes.inner}>
+        <Title className={classes.titleProps}>{t("landingPage.faqsTitle")}</Title>
+
+        <Accordion
+          classNames={{
+            item: classes.itemFaq,
+            control: classes.control,
+          }}
+        >
+          {faqs.map(({ title, description }: LandingPageFAQ) => (
+            <Accordion.Item key={title} value={title}>
+              <Accordion.Control>{title}</Accordion.Control>
+              <Accordion.Panel>
+                {description.map((item, key) => {
+                  return (
+                    <Text key={key} pb={"xs"}>
+                      {item}
+                    </Text>
+                  );
+                })}
+              </Accordion.Panel>
+            </Accordion.Item>
+          ))}
+        </Accordion>
       </Container>
     </Container>
   );
